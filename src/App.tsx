becffecf213588461c6e473ed19509910b48c62a
Munish Kumar <mkk700@gmail.com>
2023-05-18 16:18:04 -0400

    add routs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import useFetch from './hooks/useFetch';
-import LeadFormComponent from './LeadFormComponent';
-
-const url = `http://jsonplaceholder.typicode.com/posts`
-
-interface Post {
-  userId: number
-  id: number
-  title: string
-  body: string
-}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
- 
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          updated
-        </p>
-        <p>
-        <LeadFormComponent />
-        </p>
-      </header>
-    </div>
+    <Container>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Lead Gen - Continued Flow Demo</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/v1">V1</Nav.Link>
+            <Nav.Link href="/v2">V2</Nav.Link>
+            <Nav.Link href="/v3">V3</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<h2>v1</h2>} />
+          <Route path="/v1" element={<h2>v1</h2>} />
+          <Route path="/v2" element={<h2>v2</h2>} />
+          <Route path="/v3" element={<h2>v3</h2>} />
+          {/* not found */}
+          <Route path="*" element={<h2>Not Found</h2>} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
