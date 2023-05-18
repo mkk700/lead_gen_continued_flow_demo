9c85501bce22208ed9808db37df3cd8dac803953
Munish Kumar <mkk700@gmail.com>
2023-05-18 14:20:21 -0400

    fetch lead

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useFetch from './hooks/useFetch';
+import LeadFormComponent from './LeadFormComponent';
+
+const url = `http://jsonplaceholder.typicode.com/posts`
+
+interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
 
 function App() {
+ 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           updated
+        </p>
+        <p>
+        <LeadFormComponent />
         </p>
       </header>
     </div>
---
+++ src/LeadFormComponent.tsx
@@ -0,0 +1,40 @@
+import useFetch from './hooks/useFetch';
+
+const lead_id = 817930642516421;
+const access_token = 'EAAEQUasLgS4BAMb8IerAu4pRjAjvZAxZAKQTf84HVjztwVJG9iguy1iCQRTe2ZCBvG9tPvRnoTwmgeX26zew4E0hrJRYA7cjQdTK9kZBLiT8KrrKZColMDEnKZCHrtTYruDivLuOOjIRZALBQ7ApbtuboJPNnbPVgFZB0tRPas8Q8dv9mf9UsCsONFppHRvvckyulX97Ev0TrPKqB02BXRJU';
+const url = `https://graph.facebook.com/v16.0/${lead_id}?access_token=${access_token}`
+
+const options = {
+  method: 'GET',
+}
+
+interface LeadsData {
+  id: number,
+  created_time: number,
+  field_data: Array<Field>
+}
+
+export interface Field {
+  name: string;
+  values: Array<String>;
+}
+
+function LeadFormComponent() {
+  const { data, error } = useFetch<LeadsData>(url, options)
+  console.log("data: ", data);
+  if (data) {
+    data.field_data.map((field: Field) => {
+      console.log(`name: ${field.name} value: ${field.values[0]}`);
+    })
+  }
+
+  return (
+    <div className="App">
+      <p>
+        fetched lead
+      </p>
+    </div>
+  );
+}
+
+export default LeadFormComponent;
